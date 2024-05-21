07b2a3d464580a6bb234f1b66fe6efaea6ccb186
mnrx2020 <jamesmbere@gmail.com>
2024-05-21 05:23:57 +0300

    updated the change

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import "./Item.css"
-import { Link } from 'react-router-dom'
+import React from 'react';
+import "./Item.css";
+import { Link } from 'react-router-dom';
 
-
-const Item = (props) => {
+const Item = ({ id, name, image, new_price, old_price }) => {
   return (
     <div className='item'>
-      <Link to={`/product/${props.id}`}><img onClick={window.scrollTo(0,0)} src={props.image} alt='' /></Link>
-      <p>{props.name}</p>
+      <Link to={`/product/${id}`}>
+        <img onClick={() => window.scrollTo(0, 0)} src={image} alt={name} />
+      </Link>
+      <p>{name}</p>
       <div className='item-prices'>
-        <div className='item-price-new'>${props.new_price}</div>
-        <div className='item-price-old'>${props.old_price}</div>
+        <div className='item-price-new'>${new_price}</div>
+        <div className='item-price-old'>${old_price}</div>
       </div>
     </div>
-  )
+  );
 }
 
-
-export default Item
+export default Item;
