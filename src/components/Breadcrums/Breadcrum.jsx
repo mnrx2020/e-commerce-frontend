bebf5373d9145db03059830cb4fd0ba639221043
mnrx2020 <jamesmbere@gmail.com>
2024-05-21 08:24:31 +0300

    update change

--- src/components/Breadcrums/Breadcrum.jsx
+++ src/components/Breadcrums/Breadcrum.jsx
@@ -1,18 +1,27 @@
-import React from 'react';
-import "./Breadcrum.css";
-import arrow_icon from "../Assets/breadcrum_arrow.png";
+import React from 'react'
 
-const Breadcrums = ({ product }) => {
-  // Check if product is defined and has category and name properties
-  if (!product || !product.category || !product.name) {
-    return null; // or render a placeholder or loading state
-  }
+import "./Breadcrum.css"
+
+import arrow_icon from "../Assets/breadcrum_arrow.png"
+
+
+
+const Breadcrums = (props) => {
+
+  const {product} = props
 
   return (
-    <div className='breadcrum'>
-      HOME <img src={arrow_icon} alt='' /> SHOP <img src={arrow_icon} alt='' /> {product.category} <img src={arrow_icon} alt='' /> {product.name}
-    </div>
-  );
-};
 
-export default Breadcrums;
+    <div className='breadcrum'>
+
+      HOME <img src={arrow_icon} alt='' /> SHOP <img src={arrow_icon} alt='' /> {product.category} <img src={arrow_icon} alt='' /> {product.name}
+
+    </div>
+
+  )
+
+}
+
+
+
+export default Breadcrums
