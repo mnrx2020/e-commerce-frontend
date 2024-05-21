97668b829dafbeb098a98dc198647f49ac6ad8b5
mnrx2020 <jamesmbere@gmail.com>
2024-05-21 22:41:32 +0300

    update change

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -3,11 +3,9 @@ import "./Item.css";
 import { Link } from 'react-router-dom';
 
 const Item = ({ id, name, image, new_price, old_price }) => {
-  const baseUrl = process.env.NODE_ENV === 'production'
-    ? 'https://mnrx-mern-e-commerce-backend-app-api.onrender.com/images/'
-    : 'http://localhost:4000/images/';
-  
-  const imageUrl = baseUrl + image;
+  // Check if the image is a full URL or an imported image
+  const isFullUrl = typeof image === 'string' && (image.startsWith("http://") || image.startsWith("https://"));
+  const imageUrl = isFullUrl ? image : image.default || image; // For imported images, `image` would be an object
 
   return (
     <div className='item'>
