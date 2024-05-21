72600a23a8771d72f7f701495fda8a64a8dd9460
mnrx2020 <jamesmbere@gmail.com>
2024-05-21 19:08:59 +0300

    update change

--- src/components/RelatedProducts/RelatedProducts.jsx
+++ src/components/RelatedProducts/RelatedProducts.jsx
@@ -1,23 +1,34 @@
-import React from 'react'
-import "./RelatedProducts.css"
-import data_product from "../Assets/data"
-import Item from '../Item/Item'
-
+import React from 'react';
+import "./RelatedProducts.css";
+import data_product from "../Assets/data";
+import Item from '../Item/Item';
 
 const RelatedProducts = () => {
   return (
     <div className='relatedproducts'>
       <h1>Related Products</h1>
-      <hr/>
+      <hr />
       <div className='relatedproducts-item'>
-        {data_product.map((item,i)=>{
-          //const imageUrl = `https://mnrx-mern-e-commerce-backend-app-api.onrender.com/images/${item.image}`;
-            return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+        {data_product.map((item, i) => {
+          // Construct the image URL similarly
+          const isFullUrl = item.image.startsWith("http://") || item.image.startsWith("https://");
+          const baseImageUrl = isFullUrl ? "" : "https://mnrx-mern-e-commerce-backend-app-api.onrender.com/images/";
+          const imageUrl = `${baseImageUrl}${item.image}`;
+          
+          return (
+            <Item 
+              key={i} 
+              id={item.id} 
+              name={item.name} 
+              image={imageUrl} 
+              new_price={item.new_price} 
+              old_price={item.old_price} 
+            />
+          );
         })}
       </div>
     </div>
-  )
+  );
 }
 
-
-export default RelatedProducts
+export default RelatedProducts;
