aec326f39042c647c635f17e027aa2d57b8eec22
mnrx2020 <jamesmbere@gmail.com>
2024-05-21 03:52:39 +0300

    updated the changes

--- src/components/NewCollections/NewCollections.jsx
+++ src/components/NewCollections/NewCollections.jsx
@@ -1,28 +1,28 @@
-import React, { useEffect, useState } from 'react'
-import "./NewCollections.css"
-import Item from "../Item/Item"
+import React, { useEffect, useState } from 'react';
+import "./NewCollections.css";
+import Item from "../Item/Item";
 
 const NewCollections = () => {
+  const [newCollection, setNewCollection] = useState([]);
 
-  const [new_collection,setNew_collection] = useState([])
-
-  useEffect(()=>{
+  useEffect(() => {
     fetch("https://mnrx-mern-e-commerce-backend-app-api.onrender.com/newcollections")
-    .then((response)=>response.json())
-    .then((data)=>setNew_collection(data))
-  },[])
+      .then((response) => response.json())
+      .then((data) => setNewCollection(data));
+  }, []);
 
   return (
     <div className='new-collections'>
       <h1>NEW COLLECTIONS</h1>
       <hr/>
       <div className='collections'>
-        {new_collection.map((item,i)=>{
-            return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+        {newCollection.map((item, i) => {
+          const imageUrl = `https://mnrx-mern-e-commerce-backend-app-api.onrender.com/images/${item.image}`;
+          return <Item key={i} id={item.id} name={item.name} image={imageUrl} new_price={item.new_price} old_price={item.old_price} />
         })}
       </div>
     </div>
-  )
+  );
 }
 
-export default NewCollections
+export default NewCollections;
--- src/components/Pupular/Popular.jsx
+++ src/components/Pupular/Popular.jsx
@@ -1,28 +1,37 @@
-import React, { useEffect, useState } from 'react'
-import "./Popular.css"
-import Item from '../Item/Item'
+import React, { useEffect, useState } from 'react';
+import "./Popular.css";
+import Item from '../Item/Item';
 
 const Popular = () => {
+  const [popularProducts, setPopularProducts] = useState([]);
 
-  const [popularProducts,setPopularProducts] = useState([])
-
-  useEffect(()=>{
+  useEffect(() => {
     fetch("https://mnrx-mern-e-commerce-backend-app-api.onrender.com/popularinwomen")
-    .then((response)=>response.json())
-    .then((data)=>setPopularProducts(data))
-  },[])
+      .then((response) => response.json())
+      .then((data) => setPopularProducts(data));
+  }, []);
 
   return (
     <div className='popular'>
       <h1>POPULAR IN WOMEN</h1>
       <hr/>
       <div className='popular-item'>
-        {popularProducts.map((item,i)=>{
-            return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+        {popularProducts.map((item, i) => {
+          const imageUrl = `https://mnrx-mern-e-commerce-backend-app-api.onrender.com/images/${item.image}`;
+          return (
+            <Item 
+              key={i} 
+              id={item.id} 
+              name={item.name} 
+              image={imageUrl} 
+              new_price={item.new_price} 
+              old_price={item.old_price} 
+            />
+          );
         })}
       </div>
     </div>
-  )
+  );
 }
 
-export default Popular
+export default Popular;
