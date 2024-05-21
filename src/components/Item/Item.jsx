055422d20ef3b0a57743df040323499327660d90
mnrx2020 <jamesmbere@gmail.com>
2024-05-21 05:32:09 +0300

    updated the change

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -3,10 +3,15 @@ import "./Item.css";
 import { Link } from 'react-router-dom';
 
 const Item = ({ id, name, image, new_price, old_price }) => {
+  // Define the base URL for images
+  const baseImageUrl = "https://mnrx-mern-e-commerce-backend-app-api.onrender.com/images/";
+  // Construct the full image URL
+  const imageUrl = `${baseImageUrl}${image}`;
+
   return (
     <div className='item'>
       <Link to={`/product/${id}`}>
-        <img onClick={() => window.scrollTo(0, 0)} src={image} alt={name} />
+        <img onClick={() => window.scrollTo(0, 0)} src={imageUrl} alt={name} />
       </Link>
       <p>{name}</p>
       <div className='item-prices'>
