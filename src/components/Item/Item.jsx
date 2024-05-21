63b2f05b646da8d6c2b9382a4a4a8562e6164342
mnrx2020 <jamesmbere@gmail.com>
2024-05-21 18:28:30 +0300

    update change

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,25 +1,35 @@
-import React from 'react';
-import "./Item.css";
-import { Link } from 'react-router-dom';
+import React from 'react'
 
-const Item = ({ id, name, image, new_price, old_price }) => {
-  // Define the base URL for images
-  const baseImageUrl = "https://mnrx-mern-e-commerce-backend-app-api.onrender.com/images/";
-  // Construct the full image URL
-  const imageUrl = `${baseImageUrl}${image}`;
+import "./Item.css"
+
+import { Link } from 'react-router-dom'
+
+
+
+const Item = (props) => {
 
   return (
+
     <div className='item'>
-      <Link to={`/product/${id}`}>
-        <img onClick={() => window.scrollTo(0, 0)} src={imageUrl} alt={name} />
-      </Link>
-      <p>{name}</p>
+
+      <Link to={`/product/${props.id}`}><img onClick={window.scrollTo(0,0)} src={props.image} alt='' /></Link>
+
+      <p>{props.name}</p>
+
       <div className='item-prices'>
-        <div className='item-price-new'>${new_price}</div>
-        <div className='item-price-old'>${old_price}</div>
+
+        <div className='item-price-new'>${props.new_price}</div>
+
+        <div className='item-price-old'>${props.old_price}</div>
+
       </div>
+
     </div>
-  );
+
+  )
+
 }
 
-export default Item;
+
+
+export default Item
