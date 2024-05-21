101ca312f9e5d76ae5bfbb0e053637e0b1848ea5
mnrx2020 <jamesmbere@gmail.com>
2024-05-22 02:28:47 +0300

    update change

--- src/components/ProductDisplay/ProductDisplay.jsx
+++ src/components/ProductDisplay/ProductDisplay.jsx
@@ -4,14 +4,20 @@ import star_icon from "../Assets/star_icon.png";
 import star_dull_icon from "../Assets/star_dull_icon.png";
 import { ShopContext } from '../../Context/ShopContext';
 
+
 const ProductDisplay = ({ product }) => {
     const { addToCart } = useContext(ShopContext);
 
+
+    // Check if product is defined
     if (!product) {
-        return null;
+        return null; // or render a placeholder or loading state
     }
 
+
+    // Set a default value for product rating if not provided
     const rating = product.rating || 0;
+
 
     return (
         <div className='productdisplay'>
@@ -35,9 +41,11 @@ const ProductDisplay = ({ product }) => {
             <div className='productdisplay-right'>
                 <h1>{product.name}</h1>
                 <div className='productdisplay-right-stars'>
+                    {/* Render star icons based on product.rating */}
                     {[...Array(rating)].map((_, index) => (
                         <img key={index} src={star_icon} alt="" />
                     ))}
+                    {/* Render dull stars for remaining */}
                     {[...Array(5 - rating)].map((_, index) => (
                         <img key={index + rating} src={star_dull_icon} alt="" />
                     ))}
@@ -57,7 +65,7 @@ const ProductDisplay = ({ product }) => {
                         <div>M</div>
                         <div>L</div>
                         <div>XL</div>
-                        <div>XXL</div>
+                        <div>XXl</div>
                     </div>
                 </div>
                 <button onClick={() => { addToCart(product.id) }}>ADD TO CART</button>
@@ -67,5 +75,6 @@ const ProductDisplay = ({ product }) => {
         </div>
     );
 }
+
 
 export default ProductDisplay;
