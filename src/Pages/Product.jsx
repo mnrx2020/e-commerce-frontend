6e3c1517c1fb82d31b40fa2ed2677f7ca9157c36
mnrx2020 <jamesmbere@gmail.com>
2024-05-22 18:24:23 +0300

    update change

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -12,7 +12,7 @@ const Product = () => {
   const product = all_product.find((e)=>e.id===Number(productId))
   return (
     <div>
-      <Breadcrum product={product} />
+      <Breadcrum  />
       <ProductDisplay product={product}/>
       <DescriptionBox/>
       <RelatedProducts/>
