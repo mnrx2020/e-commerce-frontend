9f474f7f9dab70f3146c464b3d91c00e240b7d67
mnrx2020 <jamesmbere@gmail.com>
2024-05-22 19:00:12 +0300

    update change

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,23 +1,28 @@
-import React, { useContext } from 'react'
-import { useParams } from 'react-router-dom'
-import {ShopContext} from "../Context/ShopContext"
-import Breadcrum from '../components/Breadcrums/Breadcrum'
-import ProductDisplay from '../components/ProductDisplay/ProductDisplay'
-import DescriptionBox from '../components/DescriptionBox/DescriptionBox'
-import RelatedProducts from '../components/RelatedProducts/RelatedProducts'
+import React, { useContext } from 'react';
+import { useParams } from 'react-router-dom';
+import { ShopContext } from "../Context/ShopContext";
+import Breadcrum from '../components/Breadcrums/Breadcrum';
+import ProductDisplay from '../components/ProductDisplay/ProductDisplay';
+import DescriptionBox from '../components/DescriptionBox/DescriptionBox';
+import RelatedProducts from '../components/RelatedProducts/RelatedProducts';
 
 const Product = () => {
-  const {all_product} = useContext(ShopContext)
-  const {productId} = useParams()
-  const product = all_product.find((e)=>e.id===Number(productId))
+  const { all_product } = useContext(ShopContext);
+  const { productId } = useParams();
+  const product = all_product.find((e) => e.id === Number(productId));
+
+  if (!product) {
+    return <div>Product not found</div>;
+  }
+
   return (
     <div>
       <Breadcrum product={product} />
-      <ProductDisplay product={product}/>
-      <DescriptionBox/>
-      <RelatedProducts/>
+      <ProductDisplay product={product} />
+      <DescriptionBox />
+      <RelatedProducts />
     </div>
-  )
+  );
 }
 
-export default Product
+export default Product;
