e4d748dbdfb19297ffbe4bfa734d0152fcf47837
mnrx2020 <jamesmbere@gmail.com>
2024-05-22 17:55:12 +0300

    update change

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,31 +1,23 @@
-import React, { useContext, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { ShopContext } from "../Context/ShopContext";
-import Breadcrum from '../components/Breadcrums/Breadcrum';
-import ProductDisplay from '../components/ProductDisplay/ProductDisplay';
-import DescriptionBox from '../components/DescriptionBox/DescriptionBox';
-import RelatedProducts from '../components/RelatedProducts/RelatedProducts';
+import React, { useContext } from 'react'
+import { useParams } from 'react-router-dom'
+import {ShopContext} from "../Context/ShopContext"
+import Breadcrum from '../components/Breadcrums/Breadcrum'
+import ProductDisplay from '../components/ProductDisplay/ProductDisplay'
+import DescriptionBox from '../components/DescriptionBox/DescriptionBox'
+import RelatedProducts from '../components/RelatedProducts/RelatedProducts'
 
 const Product = () => {
-    const { all_product } = useContext(ShopContext);
-    const { productId } = useParams();
-    
-    useEffect(() => {
-        console.log("all_product:", all_product); // Log the all_product array
-    }, [all_product]);
-
-    const product = all_product.find((e) => e.id === Number(productId));
-
-    console.log("Product ID from URL:", productId); // Log the product ID from URL
-    
-    return (
-        <div>
-            <Breadcrum product={product} />
-            <ProductDisplay product={product} />
-            <DescriptionBox />
-            <RelatedProducts />
-        </div>
-    );
+  const {all_product} = useContext(ShopContext)
+  const {productId} = useParams()
+  const product = all_product.find((e)=>e.id===Number(productId))
+  return (
+    <div>
+      <Breadcrum product={product} />
+      <ProductDisplay product={product}/>
+      <DescriptionBox/>
+      <RelatedProducts/>
+    </div>
+  )
 }
 
-export default Product;
+export default Product
