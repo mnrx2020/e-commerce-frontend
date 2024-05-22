dd68dcc09e4c2f5ea75b1f1894dcc37127602e41
mnrx2020 <jamesmbere@gmail.com>
2024-05-22 21:11:17 +0300

    update change

--- src/components/CartItems/CartItems.jsx
+++ src/components/CartItems/CartItems.jsx
@@ -1,71 +1,72 @@
-import React, { useContext } from 'react'
-import "./CartItems.css"
-import { ShopContext } from '../../Context/ShopContext'
-import remove_icon from "../Assets/cart_cross_icon.png"
+import React, { useContext } from 'react';
+import "./CartItems.css";
+import { ShopContext } from '../../Context/ShopContext';
+import remove_icon from "../Assets/cart_cross_icon.png";
 
 const CartItems = () => {
-    const {getTotalCartAmount,all_product,cartItems,removeFromCart} = useContext(ShopContext)
-  return (
-    <div className='cartItems'>
-      <div className='cartItems-format-main'>
-        <p>Products</p>
-        <p>Title</p>
-        <p>Price</p>
-        <p>Quantity</p>
-        <p>Total</p>
-        <p>Remove</p>
-      </div>
-      <hr/>
-      {all_product.map((e,index)=>{
-        if(cartItems[e.id]>0){
-            return <div key={index}>
-            <div className='cartItems-format cartItems-format-main'>
-                <img src={e.image} alt="" className='carticon-product-icon' />
-                <p>{e.name}</p>
-                <p>${e.new_price}</p>
-                <button className='cartitems-quantity'>{cartItems[e.id]}</button>
-                <p>${e.new_price*cartItems[e.id]}</p>
-                <img className='cartitems-remove-icon' src={remove_icon} onClick={()=>{removeFromCart(e.id)}} alt="" />
+    const { getTotalCartAmount, all_product, cartItems, removeFromCart } = useContext(ShopContext);
+
+    return (
+        <div className='cartItems'>
+            <div className='cartItems-format-main'>
+                <p>Products</p>
+                <p>Title</p>
+                <p>Price</p>
+                <p>Quantity</p>
+                <p>Total</p>
+                <p>Remove</p>
             </div>
             <hr/>
-          </div>
-        }
-        /*else{
-            return null
-        }*/
-        return null
-      })}
-      <div className='cartitems-down'>
-        <div className='cartitems-total'>
-            <h1>cart Totals</h1>
-            <div>
-                <div className='cartitems-total-item'>
-                    <p>Subtotal</p>
-                    <p>${getTotalCartAmount()}</p>
+            {all_product.map((product, index) => {
+                if (cartItems[product.id] > 0) {
+                    const baseImageUrl = process.env.REACT_APP_BACKEND_URL + '/images/';
+                    return (
+                        <div key={index}>
+                            <div className='cartItems-format cartItems-format-main'>
+                                <img src={baseImageUrl + product.image} alt="" className='carticon-product-icon' />
+                                <p>{product.name}</p>
+                                <p>${product.new_price}</p>
+                                <button className='cartitems-quantity'>{cartItems[product.id]}</button>
+                                <p>${product.new_price * cartItems[product.id]}</p>
+                                <img className='cartitems-remove-icon' src={remove_icon} onClick={() => { removeFromCart(product.id) }} alt="" />
+                            </div>
+                            <hr/>
+                        </div>
+                    );
+                }
+                return null;
+            })}
+            <div className='cartitems-down'>
+                <div className='cartitems-total'>
+                    <h1>cart Totals</h1>
+                    <div>
+                        <div className='cartitems-total-item'>
+                            <p>Subtotal</p>
+                            <p>${getTotalCartAmount()}</p>
+                        </div>
+                        <hr/>
+                        <div className='cartitems-total-item'>
+                            <p>Shipping Fee</p>
+                            <p>Free</p>
+                        </div>
+                        <hr/>
+                        <div className='cartitems-total-item'>
+                            <h3>Total</h3>
+                            <h3>${getTotalCartAmount()}</h3>
+                        </div>
+                    </div>
+                    <button>PROCEED TO CHECKOUT</button>
                 </div>
-                <hr/>
-                <div className='cartitems-total-item'>
-                    <p>Shipping Fee</p>
-                    <p>Free</p>
-                </div>
-                <hr/>
-                <div className='cartitems-total-item'>
-                    <h3>Total</h3>
-                    <h3>${getTotalCartAmount()}</h3>
+                <div className='cartitems-promocode'>
+                    <p>If you have a promo code, Enter it here</p>
+                    <div className='cartitems-promobox'>
+                        <input type='text' placeholder='promo code' />
+                        <button>Submit</button>
+                    </div>
                 </div>
             </div>
-            <button>PROCEED TO CHECKOUT</button>
         </div>
-        <div className='cartitems-promocode'>
-            <p>If you have a promo code, Enter it here</p>
-            <div className='cartitems-promobox'>
-                <input type='text' placeholder='promo code' />
-                <button>Submit</button>
-            </div>
-        </div>
-      </div>
-    </div>
-  )
+    );
 }
 
-export default CartItems
+export default CartItems;
